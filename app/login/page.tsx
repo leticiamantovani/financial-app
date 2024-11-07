91768a00b500450bff3ad1262036b221487015f9
leticiamantovani <leticia.silva@anota.ai>
2024-11-06 22:20:44 -0300

    authentification with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
 import { Button } from "@/app/_components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { LogInIcon } from "lucide-react";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto max-w-[550px] flex flex-col h-full justify-center p-8">
@@ -19,8 +28,11 @@ const LoginPage = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-
-        <Button variant="outline"> <LogInIcon className="mr-2" /> Fazer login ou criar conta</Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" /> Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
 
       {/*DIREITA*/}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <h1>ola</h1>;
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
+  return (
+    <div className="h-full flex items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
 };
 
 export default Home;
