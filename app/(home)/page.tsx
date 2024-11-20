3bd4d2b7551b0adde30a14c5c200e350278dc311
leticiamantovani <leticiamantovani159@gmail.com>
2024-11-19 22:08:33 -0300

    feat: add user validation and default month

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -24,7 +24,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   console.log("HOME", month);
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    redirect("?month=01");
+    redirect(`/home?month=${new Date().getMonth() + 1}`);
   }
 
   const dashboard = await getDashboard(month);
