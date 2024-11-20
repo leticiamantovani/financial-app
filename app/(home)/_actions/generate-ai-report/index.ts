82fada26f387fec7d14702768ac486d646ddc2e1
leticiamantovani <leticiamantovani159@gmail.com>
2024-11-20 15:56:58 -0300

    feat: add month schema and integration with openai

---
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { endOfMonth, startOfMonth } from "date-fns";
+import OpenAIApi from "openai";
+import { generateAiReportSchema, GenerateAiReportSchema } from "./schema";
+
+const openAi = new OpenAIApi({ apiKey: process.env.OPENAI_API_KEY });
+
+export const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
+	generateAiReportSchema.parse({ month });
+	const { userId } = await auth();
+
+	if (!userId) {
+		throw new Error("User not found");
+	}
+
+	const user = await clerkClient().users.getUser(userId);
+	const hasPremiumSubscription = user.publicMetadata.subscriptionPlan === "premium";
+
+	if (!hasPremiumSubscription) {
+		throw new Error("User does not have a premium subscription");
+	}
+
+	const getCurrentMonthTransactions = await db.transaction.findMany({
+		where: {
+			date: {
+				gte: startOfMonth(`2024-${month}-01`),
+				lt: endOfMonth(`2024-${month}-31`),
+			},
+		},
+	});
+
+	const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
+  ${getCurrentMonthTransactions
+		.map(
+			(transaction) =>
+				`${transaction.date.toLocaleDateString("pt-BR")}-R$${transaction.amount}-${transaction.type}-${transaction.category}`
+		)
+		.join(";")}`;
+
+	const completion = await openAi.chat.completions.create({
+		model: "gpt-4o-mini",
+		messages: [
+			{
+				role: "system",
+				content:
+					"Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor as suas finanças.",
+			},
+			{
+				role: "user",
+				content,
+			},
+		],
+	});
+
+	return completion.choices[0].message.content;
+};
--- app/(home)/_components/ai-report-button.tsx
+++ app/(home)/_components/ai-report-button.tsx
@@ -1,42 +1,58 @@
+"use client";
+
 import { Button } from "@/app/_components/ui/button";
 import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
 } from "@/app/_components/ui/dialog";
 import { BotIcon } from "lucide-react";
+import { generateAiReport } from "../_actions/generate-ai-report";
+import { useState } from "react";
 
-const AiReportButton = () => {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="ghost" className="font-bold">
-          Relatório de IA
-          <BotIcon />
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Relatório IA</DialogTitle>
-          <DialogDescription>
-            Use a inteligência artificial para analisar suas transações e obter
-            insights valiosos.
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="ghost">Fechar</Button>
-          </DialogClose>
-          <Button>Gerar relatório</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
+interface AiReportButtonProps {
+	month: string;
+}
+
+const AiReportButton = ({ month }: AiReportButtonProps) => {
+	const [report, setReport] = useState<string | null>(null);
+	const handleGenerateAiReport = async () => {
+		try {
+			const aiReport = await generateAiReport({ month });
+			setReport(aiReport);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button variant="ghost" className="font-bold">
+					Relatório de IA
+					<BotIcon />
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Relatório IA</DialogTitle>
+					<DialogDescription>
+						Use a inteligência artificial para analisar suas transações e obter insights valiosos.
+					</DialogDescription>
+				</DialogHeader>
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button variant="ghost">Fechar</Button>
+					</DialogClose>
+					<Button onClick={handleGenerateAiReport}>Gerar relatório</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
 };
 
 export default AiReportButton;
