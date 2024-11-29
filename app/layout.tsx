94410b5e931366eace4a8bfbbd8f67ebf146152a
leticiamantovani <leticiamantovani159@gmail.com>
2024-11-29 13:38:45 -0300

    feat: delete transaction

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Mulish } from "next/font/google";
+import { Toaster } from "./_components/ui/sonner";
 
 const mulish = Mulish({
   subsets: ["latin-ext"],
@@ -24,6 +25,7 @@ export default function RootLayout({
         <ClerkProvider appearance={{ baseTheme: dark }}>
           {children}
         </ClerkProvider>
+        <Toaster />
       </body>
     </html>
   );
